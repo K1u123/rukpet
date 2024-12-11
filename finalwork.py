308a6160206fb10f8132d4703435149b85a78f3b
yura <yuraBGTY2006@gmail.com>
2024-12-11 15:07:26 +0300

    'v3'

--- finalwork.py
+++ finalwork.py
@@ -5,6 +5,41 @@ class Ticket:
         self.tickets = self.tickets
 
 
+class Admin:
+    def __init__(self, ticket):
+        self.ticket = ticket
+
+    def add_ticket(self, ticket_number, departure_point, destination, date, time):
+        self.ticket.tickets[ticket_number] = {
+            'departure point': departure_point,
+            'destination': destination,
+            'date': date,
+            'time': time
+        }
+
+    def del_ticket(self, ticket_number):
+        if ticket_number in self.ticket.tickets:
+            del self.ticket.tickets[ticket_number]
+
+    def route_change(self, ticket_number, departure_point, destination):
+        if ticket_number in self.ticket.tickets:
+            self.ticket.tickets[ticket_number]['departure point'] = departure_point
+            self.ticket.tickets[ticket_number]['destination'] = destination
+
+
+class LoggedAdmin:
+    ADMIN_PASS = 12345
+
+    def __init__(self, password):
+        self.password = password
+
+    def login(self):
+        if self.password == self.ADMIN_PASS:
+            return True
+        else:
+            return False
+
+
 class BookingSystem:
     def __init__(self, ticket, admin):
         self.logged_admin = admin
@@ -33,7 +68,7 @@ class BookingSystem:
 
 ticket = Ticket()
 admin = Admin(ticket)
-logged_admin = LoggedAdmin(1235)
+logged_admin = LoggedAdmin(12435)
 
 if logged_admin.login():
     admin.add_ticket('123', 'Москва', 'Пекин', '2022-12-31', '10:00')
